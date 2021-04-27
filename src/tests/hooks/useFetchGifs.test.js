b39988d33dcb25ef39452b647a479c4a3c2c1083
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2021-04-27 08:14:16 -0500

    Test en customHook useFetchGifs

---
+++ src/tests/hooks/useFetchGifs.test.js
@@ -0,0 +1,37 @@
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+import { renderHook } from '@testing-library/react-hooks'
+
+describe('Pruebas en el hook useFetchGifs', () => {
+
+    test('debe de retornar el estado inicial', async () => {
+
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'One Punch' ) );
+        const {data, loading } = result.current;
+        
+        await waitForNextUpdate();
+
+
+        expect( data ).toEqual( [] );
+        expect( loading ).toBe( true );
+
+        
+    });
+    
+    test('debe de retornar un arreglo de imgs y el loading en false', async () => {
+        
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'One Punch' ) );
+        
+        await waitForNextUpdate();
+
+        const { data:images, loading } = result.current;
+    
+        expect( images.length ).toBe( 10 );
+        expect( loading ).toBe( false );
+    });
+    
+    
+    
+    
+    
+    
+})
